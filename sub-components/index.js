a6a43b22bd8a26260b13db6478bb03d98319c589
lesterdgreat <lester010309@yahoo.com>
2023-07-26 18:09:53 +0800

    Remove unnecessary code

--- sub-components/index.js
+++ sub-components/index.js
@@ -3,11 +3,6 @@
  * so here you will find folder names which are listed in root pages.
  */
 
-// sub components for /pages/dashboard
-import ActiveProjects from 'sub-components/dashboard/ActiveProjects';
-import TasksPerformance from 'sub-components/dashboard/TasksPerformance';
-import Teams from 'sub-components/dashboard/Teams';
-
 // sub components for /pages/profile
 import AboutMe from 'sub-components/profile/AboutMe';
 import ActivityFeed from 'sub-components/profile/ActivityFeed';
@@ -29,10 +24,6 @@ import Preferences from 'sub-components/settings/Preferences';
 
 
 export {
-   ActiveProjects,
-   TasksPerformance,
-   Teams,
-   
    AboutMe,
    ActivityFeed,
    MyTeam,
